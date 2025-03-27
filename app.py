c40efd5871730226d29b2e67db5d462e0e06447a
Matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2025-03-27 11:31:45 +0100

    Update redirect logic

--- app.py
+++ app.py
@@ -9,11 +9,31 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def extract_urls_from_text(text_content):
+
+def extract_urls_from_text(text_content) -> list[str]:
     """Extracts URLs from plain text content using regex."""
     return re.findall(r'https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#]*)(?![\/])', text_content)
+    
 
-def is_url_reachable(url):
+def get_urls_list(request_url: str):
+    try:
+        urls = []
+        response = requests.get(request_url, timeout=10)
+        
+        for item in extract_urls_from_text(response.text):
+            if item.startswith("http://localhost") or item.startswith("http://127.0.0.1"):
+                logging.warning("URLS_LIST points to the application itself. Skipping.")
+            else:
+                urls.append(item)
+        logging.info(f"Extracted URLs from URLS_LIST: {urls}")
+        return urls
+    except Exception as e:
+        logging.error(f"Error fetching or parsing URLS_LIST: {e}")
+    return []
+
+
+def is_url_reachable(url: str):
+    url = url.strip().removesuffix('/')
     logging.info(f'Checking {url}...')
     try:
         head_request = requests.head(url, timeout=5)
@@ -22,49 +42,35 @@ def is_url_reachable(url):
     except:
         return False, url
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def redirect_to_target(path):
-    target_urls = []
-    urls_list_url = os.environ.get('URLS_LIST')
 
-    if urls_list_url:
-        try:
-            # Prevent infinite loop if URLS_LIST points to itself
-            if urls_list_url.startswith("http://localhost") or urls_list_url.startswith("http://127.0.0.1"):
-                logging.warning("URLS_LIST points to the application itself. Skipping.")
-            else:
-                response = requests.get(urls_list_url, timeout=10)
-                response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
-                urls_from_text = extract_urls_from_text(response.text)
-                target_urls.extend(urls_from_text)
-                logging.info(f"Extracted URLs from URLS_LIST: {urls_from_text}")
-        except requests.RequestException as e:
-            logging.error(f"Error fetching or parsing URLS_LIST: {e}")
-
-    target_urls_str = os.environ.get('TARGET_URLS')
-    if target_urls_str:
-        target_urls.extend([url.strip() for url in target_urls_str.split(',')])
-    
-    if not target_urls:
-        return "No target URLs found", 503
-
+def try_redirect(urls: list[str]):
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        futures = [executor.submit(is_url_reachable, url) for url in target_urls]
+        futures = [executor.submit(is_url_reachable, url) for url in urls]
         for future in concurrent.futures.as_completed(futures):
             success, url = future.result()
-            logging.info(f'{url}: {success}')
+            logging.info(f'Requested {url}: {success}')
             if success:
                 logging.info(f"Redirecting to: {url}")
                 executor.shutdown(wait=False, cancel_futures=True)  # Stop other futures as soon as we have a success
-                return redirect(url + "/" + path, code=301)
+                return url
+    return None
 
-    logging.error(f"No working target URLs found from: {target_urls}")
-    first_url = target_urls[0] if target_urls else None
-    if first_url:
-        return redirect(first_url + "/" + path, code=302)
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def redirect_to_target(path):
+    
+    if target_urls_str := os.environ.get('TARGET_URLS'):
+        if ret := try_redirect(target_urls_str.split(',')):
+            return redirect(ret + "/" + path, code=301)
+    
+    elif urls_list_url := os.environ.get('URLS_LIST'):
+        if ret := try_redirect(get_urls_list(urls_list_url)):
+            return redirect(ret + "/" + path, code=301)
+    
+    logging.error(f"No working target URLs found. Check logs.")
     return "No working target URL found", 503
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
--- readme.md
+++ readme.md
@@ -1,8 +1,18 @@
-Simple redirect server.
+# Simple redirect server.
+
+Redirects url to external domain, keeping original path
 
 - Pass URL list or a single URL as TARGET_URLS
 - Pass URL containing that URL list as URLS_LIST
 - Redirect Server will redirect to the first server that responds. Useful for services with multiple instances
+
+## Behaviour:
+Requests are split by groups of 4.
+- Checking urls provided by TARGET_URLS
+  - returns if possible
+- Checking urls provided by URLS_LIST
+  - returns if possible
+- returns error otherwise
 
 Usage:
 ```yml
